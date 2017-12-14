41b7a54ca082bd3cf962c23aef3f11ef67d70b9b
Hélène GAILLARD <gaillardhelene@gmail.com>
2017-12-14 11:30:41 +0100

    Ajout liste contacts via faker & route GET

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log('Hello World !');
+const express = require('express');
+const bodyParser = require('body-parser');
+//Installé en version de dev uniquement
+const faker = require('faker');
+
+const app = express();
+const users = [];
+const contacts = [];
+
+//Version de l'application
+const version = 'v1';
+
+/* Interprète une valeur postée par l'utilisateur ; la transforme en objet json */
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+
+/* on simule une BDD avec des utilisateurs grâce à faker */
+for (let i = 0; i < 10; i++) {
+    contacts.push({
+        email:  faker.internet.email(),
+        password: faker.internet.password(),
+    });
+}
+
+console.log(contacts);
+
+
+// GET /v1/contacts : récupère la liste de tous les contacts
+app.get(`/${version}/contacts`, (req, res) => {
+    res.json(contacts);
+});
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
